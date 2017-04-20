fce138eb20051d29c32770b344ebbee43a081a1d
Kapilesh <kapilesh@witworks.com>
2017-04-20 13:03:19 +0530

    Remove weather service and witai service

--- app/src/main/java/kapil/voiceassistedweatherapp/weather/WeatherServiceInputProvider.java
+++
@@ -1,9 +0,0 @@
-package kapil.voiceassistedweatherapp.weather;
-
-/**
- * Created by kapil on 08-01-2017.
- */
-public interface WeatherServiceInputProvider {
-    void setOnWeatherDataReceivedListener(OnWeatherDataReceivedListener onWeatherDataReceivedListener);
-    void getWeatherInfo(String place);
-}
